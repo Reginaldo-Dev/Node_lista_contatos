ab2943a1d6a6c69e94d60af3dd3dbc82e0532cb3
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-08-14 17:07:32 -0300

    Corrigindo servidor com escuta nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import helmet from "helmet";
 import router from './routes';
+import { createServer } from "node:http";
 
 const server = express();
+
 
 server.use(helmet());
 server.use(express.json());
@@ -11,5 +13,5 @@ server.use(express.urlencoded({ extended: true}));
 server.use('/', router);
 
 server.listen(3000, () => {
-    console.log('Servidor rodando: http://localhost:3000');
+    console.log('Sevidor rodando no endereço http://localhost:3000');
 });
