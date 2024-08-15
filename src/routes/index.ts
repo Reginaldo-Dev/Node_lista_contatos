5abf88905bf3376159e24dea2bac276a342ab0a4
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-08-15 16:53:13 -0300

    Fnalizando alterações

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,39 +1,35 @@
 import express from 'express';
-import { createContact, deleteContact, getContacts } from '../services/contacts';
-
+import { createContact, deleteContact, getContacts } from '../services/contact';
 
 const router = express.Router();
 
-router.post('/contato', async(req, res) => {
-    const {name} = req.body; // buscando a constante name no corpo da lista
+router.post('/contato', async (req, res) => {
+    const { name } = req.body;
 
-    //condição para que isso aconteça
-    if(!name || name.length < 2) {
-        return res.json({error: 'Nome Precisa de pelo menos 2 caracteres.'});
+    if (!name || name.length < 2) {
+        return res.json({ error: 'Nome precisa ter pelo menos 2 caracteres.' });
     }
-    
+
     await createContact(name);
 
-    res.status(201).json({ contato: name});
+    res.status(201).json({ contato: name });
 });
-// Pegando lista de contatos
+
 router.get('/contatos', async (req, res) => {
     let list = await getContacts();
+    res.json({ contatos: list });
+});
 
-    res.json( {contatos: list});
-})
-// Excluindo contato de uma lista
 router.delete('/contato', async (req, res) => {
     const { name } = req.query;
 
-    if(!name) { 
-        return res.json( { error: 'O nome não encontra na lista'});
+    if (!name) {
+        return res.json({ error: 'Precisa mandar um nome para excluir.' });
     }
 
     await deleteContact(name as string);
 
-    res.json( {contatos: list});
-})
-
+    res.json({ contato: name });
+});
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
-import express from "express";
-import helmet from "helmet";
+import express from 'express';
+import helmet from 'helmet';
 import router from './routes';
-import { createServer } from "node:http";
 
 const server = express();
 
-
 server.use(helmet());
 server.use(express.json());
-server.use(express.urlencoded({ extended: true}));
+server.use(express.urlencoded({ extended: true }));
 
 server.use('/', router);
 
 server.listen(3000, () => {
-    console.log('Sevidor rodando no endereço http://localhost:3000');
+    console.log('Servidor rodando: http://localhost:3000/');
 });
