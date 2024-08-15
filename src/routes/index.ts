849b85163fe6046ff21ae1c703ce9b32885707ee
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-08-15 15:33:13 -0300

    Inserindo dados em uma lista

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { readFile, writeFile } from 'fs/promises';
+
+const dataSource = './data/list.txt';
 
 const router = express.Router();
 
-router.post('/contato', (req, res) => {
+router.post('/contato', async(req, res) => {
     const {name} = req.body; // buscando a constante name no corpo da lista
 
     //condição para que isso aconteça
@@ -10,13 +13,18 @@ router.post('/contato', (req, res) => {
         return res.json({error: 'Nome Precisa de pelo menos 2 caracteres.'});
     }
     // processamento dos dados
+    let list: string[] = [];
+
+    try{
+        const data = await readFile(dataSource, { encoding: 'utf8'});
+        list = data.split('\n');
+        
+    } catch(err) { }
+
+    list.push(name);
+    await writeFile(dataSource, list.join('/n'));
 
     res.status(201).json({ contato: name});
-})
-
-
-
-
-
+});
 
 export default router;
